6d7b9a8524fe8ac4604747ac0941777a3cbafeb8
VB <vijaykborkar@outlook.com>
2018-09-06 12:22:46 +0530

    Update in if-elif-else statement

---
+++ 04-ControlStatements/06-Indian_Cricket_JerseyNo.py
@@ -0,0 +1,21 @@
+#Jersey Number is denotated as jno#
+
+jno = int(input("Please enter the Jersey Number of player and I'll tell you his Name:  "))
+
+if jno==7: print("Mahindra Singh Dhoni")
+elif jno==18: print("Virat Kholi")
+elif jno==10: print("Sachin Tendulkar")
+elif jno==00: print("Virendra Sehwag")
+elif jno==5: print("Gautam Gambhir")
+elif jno==25: print("Shikar Dhawan")
+elif jno==17: print("Ajinkya Rahane")
+elif jno==3: print("Suresh Raina")
+elif jno==26: print("Murali Vijay")
+elif jno==16: print("Cheteshwar Pujara")
+elif jno==45: print("Rohit Sharma")
+elif jno==8: print("Ravindra Jadeja")
+elif jno==12: print("Yuvraj Singh")
+elif jno==19: print("Dinesh Karthik")
+else:
+    print("This list is still updating... So your player will appear soon.")
+
