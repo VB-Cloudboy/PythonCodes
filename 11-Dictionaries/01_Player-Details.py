03429ad7a06295eb0b9217d9bf29da4d1b37f355
VB <vijaykborkar@outlook.com>
2018-10-09 10:00:22 +0530

    Updated Dictionaries

---
+++ 11-Dictionaries/01_Player-Details.py
@@ -0,0 +1,12 @@
+# Kapil Dev Personal Information
+
+kd = {'Name': 'Kapil Dev Ram Lal Nikhanj', 'Born': '6 January 1959' , 'Age': '59', 'Batting': 'Right Handed', 'Bowling': 'Right Arm Fast', 'Role': 'Bowling All-rounder'}
+
+#Fetching the values by feeding key details
+print('Name of the Player = ', kd['Name'])
+print ('Born On = ', kd['Born'])
+print('His Age = ', kd['Age'])
+print('Batting Style = ', kd['Batting'])
+print('Bowling Style = ', kd['Bowling'])
+print('Role in Team = ', kd['Role'])
+
