7197a23966d4423e48bba614986c7a2ce9bef70c
VB <vijaykborkar@outlook.com>
2019-02-22 16:25:55 +0530

    Operator Content is updated

---
+++ 02-Operators/01-Relational_Operators.py
@@ -0,0 +1,9 @@
+# 1. Satisfying Relational Operator condition
+
+India = 1;
+Australia = 2;
+
+if (India>Austalia):
+    print("India is ahead...")
+else:
+    print("India is still in race...")
---
+++ 02-Operators/02-Relational_Operators.py
@@ -0,0 +1,6 @@
+# 1. Satisfying Membership Operator condition
+
+Team = ["Dhoni","Yuvraj","Sachin","Virat"]
+
+for India in Team:
+    print(India)
